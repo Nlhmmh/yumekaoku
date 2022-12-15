3b6ec0ec269500d1cb1a171bb9fdff3ad1e487d1
unknown <ayemyatthinaung.1510@gmail.com>
2022-12-15 19:47:37 +0630

    Add sql folder and ddl.sql file

---
+++ backend/sqls/ddl.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS `rental_house`;
+CREATE DATABASE IF NOT EXISTS `rental_house` CHARACTER SET UTF8MB4 COLLATE UTF8MB4_GENERAL_CI;
+USE `rental_house`;
+
+CREATE TABLE `users` (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    phone_number VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+    role ENUM('admin','user') NOT NULL DEFAULT 'admin',
+    status ENUM('active','deactivated') NOT NULL DEFAULT 'active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE `categories` (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO `categories` (id, name, created_at) VALUES(1, '1LDK Apartment', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(2, '2LDK Apartment', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(3, '3LDK Apartment', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(4, '1K Apartment', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(5, '1R Apartment', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(6, 'Retail shop', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(7, 'Sharedhouse', CURRENT_TIMESTAMP());
+INSERT INTO `categories` (id, name, created_at) VALUES(8, 'Office', CURRENT_TIMESTAMP());
+
+CREATE TABLE `estates` (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    rent_fee DOUBLE NOT NULL DEFAULT 0,
+    maintenance_fee DOUBLE NOT NULL DEFAULT 0,
+    location VARCHAR(255) NOT NULL,
+    size INT,
+    is_rent_out BOOLEAN DEFAULT false,
+    remarks VARCHAR(255),
+    category_id INT UNSIGNED,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE `appointments` (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    phone_number VARCHAR(100) NOT NULL,
+    appointment_date DATETIME NOT NULL,
+    message VARCHAR(255),
+    user_id INT UNSIGNED,
+    estate_id INT UNSIGNED,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (estate_id) REFERENCES estates (id)
+);
