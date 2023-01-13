8bd229f570ee54d517b2fe7a1a473eb6a8ab2c29
Nlhmmh <nlh2012.8@gmail.com>
2023-01-13 11:25:34 +0630

    nay - sql delete

--- backend/sqls/dml.sql
+++
@@ -1,3 +0,0 @@
-USE rental_house;
-INSERT INTO user (id, email, name, password, phone_number, role, status, created_at, updated_at) 
-VALUES (1000, 'admin@gmail.com', 'Admin', '$2a$10$kEw.ip.VcctOYgsHrRldEexoLybDptm92EWnPGs19rQYCh72n6L46', '08099992222', 'admin', 'active', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
